468ca0810d5c9e3c4dd9e5f5eac5f7ffdf12ba1a
gopalkhandale1994 <140168693+gopalkhandale1994@users.noreply.github.com>
2024-07-15 19:24:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,15 @@
 variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
-  default     = var.project_id != "" ? var.project_id : "default"
 }
 
 variable "region" {
   description = "The region to deploy to"
   type        = string
-  default     = var.region != "" ? var.region : "us-central1"
+  default     = "us-central1"
 }
 
 variable "image" {
   description = "The Docker image to deploy"
   type        = string
-  default     = var.image != "" ? var.image : "us-central1-docker.pkg.dev/avian-bricolage-429512-k8/my-repo/hello-world:latest"
 }
